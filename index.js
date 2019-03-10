c75c19a97be22e8f9ab3da86e44afcfcf9e6dad6
Claudio Cortese <claudio.cortese@outlook.it>
2019-03-10 16:00:54 +0100

    test: add functional tests

--- index.js
+++ index.js
@@ -3,34 +3,32 @@ const helmet = require('helmet')
 const cors = require('cors')
 const db = require('./models').sequelize
 
-db.authenticate()
-  .then(() => {
-    const { PORT } = process.env
+db.authenticate().catch(err => {
+  console.error('Unable to connect to the database: ', err)
+})
 
-    const boardsRouter = require('./routes/boards')
-    const threadsRouter = require('./routes/threads')
-    const repliesRouter = require('./routes/replies')
+const { PORT } = process.env
 
-    module.exports = express()
-      .use(helmet())
-      .use(cors())
-      .use(express.json())
-      .use(
-        express.urlencoded({
-          extended: false,
-        })
-      )
-      .use((req, res, next) => {
-        req.db = db
-        next()
-      })
-      .use('/api/boards', boardsRouter)
-      .use('/api/threads', threadsRouter)
-      .use('/api/replies', repliesRouter)
-      .listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`)
-      })
+const boardsRouter = require('./routes/boards')
+const threadsRouter = require('./routes/threads')
+const repliesRouter = require('./routes/replies')
+
+module.exports = express()
+  .use(helmet())
+  .use(cors())
+  .use(express.json())
+  .use(
+    express.urlencoded({
+      extended: false,
+    })
+  )
+  .use((req, res, next) => {
+    req.db = db
+    next()
   })
-  .catch(err => {
-    console.error('Unable to connect to the database: ', err)
+  .use('/api/boards', boardsRouter)
+  .use('/api/threads', threadsRouter)
+  .use('/api/replies', repliesRouter)
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
   })
--- tests/functional-tests.js
+++ tests/functional-tests.js
@@ -6,23 +6,148 @@ const server = require('../index')
 chai.use(chaiHttp)
 
 suite('Functional Tests', function() {
-  suite('API ROUTING FOR /api/threads/:board', function() {
-    suite('POST', function() {})
+  suite('API ROUTING FOR /api/threads/:board', () => {
+    const delete_password = '1234'
+    let thread_id
 
-    suite('GET', function() {})
+    suite('GET', () => {
+      test('List recent threads', done => {
+        chai
+          .request(server)
+          .get('/api/threads/general')
+          .end((err, { status, body }) => {
+            assert.equal(status, 200)
+            assert.isArray(body)
+            assert.containsAllKeys(body[0], [
+              'id',
+              'text',
+              'createdAt',
+              'updatedAt',
+              'BoardId',
+              'Replies',
+            ])
+            assert.isArray(body[0].Replies)
+            done()
+          })
+      })
+    })
 
-    suite('DELETE', function() {})
+    suite('POST', () => {
+      test('Create thread', done => {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .set('content-type', 'application/json')
+          .send({ text: 'Example test', delete_password })
+          .end((err, { status, body }) => {
+            thread_id = body.id
 
-    suite('PUT', function() {})
+            assert.equal(status, 201)
+
+            assert.containsAllKeys(body, ['id', 'text', 'createdAt', 'updatedAt'])
+            done()
+          })
+      })
+    })
+
+    suite('PUT', () => {
+      test('Report thread', done => {
+        chai
+          .request(server)
+          .put('/api/threads/general')
+          .set('content-type', 'application/json')
+          .send({ thread_id })
+          .end((err, { status, body }) => {
+            assert.equal(status, 200)
+            assert.equal(body.message, 'Success')
+            done()
+          })
+      })
+    })
+
+    suite('DELETE', () => {
+      test('Delete thread with password', done => {
+        chai
+          .request(server)
+          .delete('/api/threads/general')
+          .set('content-type', 'application/json')
+          .send({ thread_id, delete_password })
+          .end((err, { status, body }) => {
+            assert.equal(status, 200)
+            assert.equal(body.message, 'Success')
+            done()
+          })
+      })
+    })
   })
 
-  suite('API ROUTING FOR /api/replies/:board', function() {
-    suite('POST', function() {})
+  suite('API ROUTING FOR /api/replies/:board', () => {
+    const delete_password = '1234'
+    let reply_id
 
-    suite('GET', function() {})
+    suite('GET', () => {
+      test('Show all replies on thread', done => {
+        chai
+          .request(server)
+          .get('/api/replies/general')
+          .query({ thread_id: 1 })
+          .end((err, { status, body }) => {
+            assert.equal(status, 200)
 
-    suite('PUT', function() {})
+            assert.isArray(body)
+            done()
+          })
+      })
+    })
 
-    suite('DELETE', function() {})
+    suite('POST', () => {
+      test('Create reply on thread', done => {
+        chai
+          .request(server)
+          .post('/api/replies/general')
+          .set('content-type', 'application/json')
+          .send({ thread_id: 1, text: 'Example reply', delete_password })
+          .end((err, { status, body }) => {
+            reply_id = body.id
+
+            assert.equal(status, 201)
+            assert.containsAllKeys(body, ['id', 'text', 'createdAt', 'updatedAt'])
+
+            done()
+          })
+      })
+    })
+
+    suite('PUT', () => {
+      test('Report reply on thread', done => {
+        chai
+          .request(server)
+          .put('/api/replies/general')
+          .set('content-type', 'application/json')
+          .send({ reply_id, delete_password })
+          .end((err, { status, body }) => {
+            assert.equal(status, 200)
+            assert.equal(body.reported, true)
+
+            done()
+          })
+      })
+    })
+
+    suite('DELETE', () => {
+      test('Change reply to `[deleted]` on thread', done => {
+        chai
+          .request(server)
+          .delete('/api/replies/general')
+          .set('content-type', 'application/json')
+          .send({ reply_id, delete_password })
+          .end((err, { status, body }) => {
+            assert.equal(status, 200)
+            assert.equal(body.message, 'Success')
+
+            done()
+          })
+      })
+    })
   })
 })
